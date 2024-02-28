1b95e0bc0dc9fc7ce78cd4000cd37fdb030ca30f
Suryaprasath <suryajack27@gmail.com>
2024-02-28 22:02:07 +0530

    HoverEffectUpdate

--- star-rating/script.js
+++ star-rating/script.js
@@ -3,6 +3,7 @@ let ratingCount = document.querySelector('#star-rating')
 
 let stars = document.querySelectorAll('.star')
 
+let filled = 0;
 starCont.addEventListener('click', (event) => {
     let currentClickedStart = event.target.dataset.index
 
@@ -21,4 +22,28 @@ starCont.addEventListener('click', (event) => {
     // }
 
     ratingCount.innerText = currentClickedStart
+    filled = currentClickedStart
 })
+
+starCont.addEventListener("mouseover", function (e) {
+    let currHovered = e.target.dataset.index;
+    
+    for (let i = 0; i < stars.length; i++) {
+    stars[i].classList.remove("star-colored");
+    }
+    
+    for (let i = 0; i < currHovered; i++) {
+    stars[i].classList.add("star-colored");
+    }
+    });
+    
+    starCont.addEventListener("mouseleave", function (e) {
+    for (let i = 0; i < stars.length; i++) {
+    stars[i].classList.remove("star-colored");
+    }
+    
+    for (let i = 0; i < filled; i++) {
+    stars[i].classList.add("star-colored");
+    }
+    });
+    
